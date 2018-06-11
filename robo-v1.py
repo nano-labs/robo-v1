fae90d4c9ff1b61b1ad79989b4d7e0cb04ca73cd
Fabio Pacheco <nanook.labs@gmail.com>
2018-06-11 01:11:39 +0100

    shutdown signal

--- robo-v1.py
+++ robo-v1.py
@@ -1,10 +1,13 @@
 import os
 from time import sleep
+from datetime import datetime
 import serial
 import pygame
 from pygame.locals import *
 import traceback
 
+reboot_signal = [datetime.now(), 0]
+shutdown_signal = [datetime.now(), 0]
 while True:
     try:
         os.putenv('DISPLAY', ':0.0')
@@ -17,10 +20,27 @@ while True:
             while True:
                 for event in pygame.event.get():
                     if event.type == pygame.KEYDOWN:
-                        ser.write(chr(event.key))
+                        # ser.write(chr(event.key))
                         print "down ", event.key
+                        if chr(event.key) == "r":
+                            if (datetime.now() - reboot_signal[0]).total_seconds() <= 3:
+                                reboot_signal[1] += 1
+                            else:
+                                reboot_signal = [datetime.now(), 0]
+                            if reboot_signal[1] >= 3:
+                                print "reboot"
+                                os.system('sudo shutdown -r now')
+                        if chr(event.key) == "t":
+                            if (datetime.now() - shutdown_signal[0]).total_seconds() <= 3:
+                                shutdown_signal[1] += 1
+                            else:
+                                shutdown_signal = [datetime.now(), 0]
+                            if shutdown_signal[1] >= 3:
+                                print "shutdown"
+                                os.system('sudo shutdown -h now')
+
                     if event.type == pygame.KEYUP:
-                        ser.write(chr(event.key).upper())
+                        # ser.write(chr(event.key).upper())
                         print "up ", event.key
     except:
         trace = traceback.format_exc()
